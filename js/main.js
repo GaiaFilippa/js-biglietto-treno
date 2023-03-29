6e942469d6e12581cc567ca46406f0bb9310a4f4
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-03-29 14:07:39 +0200

    set up index.html e main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni, calcola il prezzo totale del viaggio.
+Secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+*/
+
+let routeKm = prompt("Inserisci il numero di chilometri del tuo percorso");
